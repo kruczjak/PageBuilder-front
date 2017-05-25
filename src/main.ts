0668f7bec56948c50e6a6a364efe01bcc0a23ac6
kruczjak <kruczjak@gmail.com>
2017-05-25 20:30:14 +0200

    Add minimal pace plugin

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { environment } from './environments/environment';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
 
+import 'pace';
+
 if (environment.production) {
   enableProdMode();
 }
