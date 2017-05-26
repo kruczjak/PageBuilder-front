b388afcec97464fcd24a3ca550b55133fd9227f7
kruczjak <kruczjak@gmail.com>
2017-05-26 15:39:53 +0200

    #5 Project directory tree view

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 import 'pace';
 
